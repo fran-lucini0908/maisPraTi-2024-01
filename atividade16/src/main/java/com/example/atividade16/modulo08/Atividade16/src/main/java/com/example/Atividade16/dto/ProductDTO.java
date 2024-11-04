a5ce82de6ef83d333dacd607b1e253c97fe16539
Francieli Lucini <146274537+fran-lucini0908@users.noreply.github.com>
2024-11-04 15:21:41 -0300

    Add files via upload

---
+++ atividade16/src/main/java/com/example/atividade16/modulo08/Atividade16/src/main/java/com/example/Atividade16/dto/ProductDTO.java
@@ -0,0 +1,12 @@
+package com.example.Atividade16.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+  private String name;
+  private Double price;
+  private String description;
+  private Integer quantity;
+
+}
